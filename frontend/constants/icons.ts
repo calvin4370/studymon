15056e3387115cc1d80ef3566a40b247a21267c2
calvin4370 <calvin.j.j.chan@gmail.com>
2025-06-15 14:55:10 +0800

    feat(assets): Add assets added in previous commit (faculty icons) to constants

--- frontend/constants/icons.ts
+++ frontend/constants/icons.ts
@@ -5,6 +5,17 @@ import gear from '@/assets/icons/gear.png';
 import person from '@/assets/icons/person.png';
 import menu from '@/assets/icons/menu.png';
 
+// Faculty Icons
+import arts from '@/assets/icons/arts.png';
+import science from '@/assets/icons/science.png';
+import engineering from '@/assets/icons/engineering.png';
+import business from '@/assets/icons/business.png';
+import computing from '@/assets/icons/computing.png';
+import law from '@/assets/icons/law.png';
+import healthcare from '@/assets/icons/healthcare.png';
+import music from '@/assets/icons/music.png';
+import neutral from '@/assets/icons/neutral.png';
+
 const icons = {
   calendar,
   cards,
@@ -12,6 +23,17 @@ const icons = {
   gear,
   person,
   menu,
+
+  // Faculties
+  arts,
+  science,
+  engineering,
+  business,
+  computing,
+  law,
+  healthcare,
+  music,
+  neutral,
 };
 
 export default icons;
