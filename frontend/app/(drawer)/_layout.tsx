e1eb33bbcaea88ba2a87e30853c5f8da4393c588
gabriel-lll <gabrielsim404@gmail.com>
2025-06-20 03:36:22 +0800

    style: Removed unnecessary imports

--- frontend/app/(drawer)/_layout.tsx
+++ frontend/app/(drawer)/_layout.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import {Drawer} from "expo-router/drawer";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
-import HeaderBar from "@/components/HeaderBar";
-import { SafeAreaView } from 'react-native-safe-area-context';
 import CustomDrawer from "@/components/CustomDrawer";
 
 const DrawerLayout = () => {
