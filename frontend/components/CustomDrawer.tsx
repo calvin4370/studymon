19c1b73b7c9fee6170329f7a1c5feae7989fa417
gabriel-lll <gabrielsim404@gmail.com>
2025-06-22 23:56:26 +0800

    feat(drawer): Added design for drawer and store page

--- frontend/components/CustomDrawer.tsx
+++ frontend/components/CustomDrawer.tsx
@@ -2,6 +2,7 @@ import {Text} from 'react-native'
 import React from 'react'
 import {Drawer} from "expo-router/drawer";
 import colors from "@/constants/colors";
+import {Ionicons} from "@expo/vector-icons";
 
 const CustomDrawer = () => {
   return (
@@ -17,11 +18,57 @@ const CustomDrawer = () => {
             StudyMon
           </Text>
         ),
+        drawerStyle: {
+          backgroundColor: colors.primary,
+          width: 200,
+        },
+        drawerType: 'front',
+        drawerLabelStyle: {
+          color: colors.text,
+          fontSize: 12,
+          fontWeight: 'bold',
+        },
+        drawerContentContainerStyle: {
+          paddingTop: 105,
+        }
       }}
     >
-      <Drawer.Screen name="(tabs)" options={{ title: 'Home' }} />
-      <Drawer.Screen name="FriendsScreen" options={{ title: 'Friends' }} />
-      <Drawer.Screen name="AchievementsScreen" options={{ title: 'Achievements' }} />
+      <Drawer.Screen
+        name="(tabs)"
+        options={{
+          title: 'Home',
+          drawerIcon: () => (
+            <Ionicons name="home" size={24} color={colors.text} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="StoreScreen"
+        options={{
+          title: 'Store',
+          drawerIcon: () => (
+            <Ionicons name="cart" size={24} color={colors.text} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="FriendsScreen"
+        options={{
+          title: 'Friends',
+          drawerIcon: () => (
+            <Ionicons name="people" size={24} color={colors.text} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="AchievementsScreen"
+        options={{
+          title: 'Achievements',
+          drawerIcon: () => (
+            <Ionicons name="trophy" size={24} color={colors.text} />
+          ),
+        }}
+      />
     </Drawer>
   )
 }
