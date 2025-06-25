104dd0a82a2735f84e36e5bec7442c125c5e3e41
calvin4370 <calvin.j.j.chan@gmail.com>
2025-06-25 11:50:51 +0800

    fix(CollectionCountPill): Remove duplicated code

--- frontend/app/(drawer)/(tabs)/CollectionScreen.tsx
+++ frontend/app/(drawer)/(tabs)/CollectionScreen.tsx
@@ -1,12 +1,3 @@
-import CollectionCountPill from '@/components/CollectionCountPill';
-import { View, Text } from 'react-native';
-
-const CollectionScreen = () => {
-  return (
-    <View className='flex-1 bg-background mt-[20px] mx-[20px]'>
-      <CollectionCountPill />
-      <View className='items-center border-red-500 border-[2px]'>
-        <Text className='text-text text-[48px] font-bold'>To Be Done</Text>
 import CollectionCountPill from "@/components/CollectionCountPill";
 import { View, Text } from "react-native";
 
