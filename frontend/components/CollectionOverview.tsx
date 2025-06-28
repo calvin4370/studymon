38c65711545ff3b76c4c61080874cb6d694e153d
calvin4370 <calvin.j.j.chan@gmail.com>
2025-06-28 23:49:43 +0800

    feat(collection): Update Open Packs Button to redirect to Store screen

--- frontend/components/CollectionOverview.tsx
+++ frontend/components/CollectionOverview.tsx
@@ -1,7 +1,8 @@
 import { Text, View } from 'react-native';
 import React from 'react';
 import CollectionCountPill from './CollectionCountPill';
-import OpenPacksButton from './OpenPacksButton';
+import { router } from 'expo-router';
+import IconButtonTwoRows from './IconButtonTwoRows';
 import SwitchButton from './SwitchButton';
 import SearchButton from './SearchButton';
 
@@ -10,6 +11,15 @@ interface CollectionOverviewProps {
 }
 
 const CollectionOverview = ({ className = '' }: CollectionOverviewProps) => {
+  // Functions for onEvent
+  const onOpenPacksButtonPress = () => {
+    router.push('/CollectionScreen');
+  };
+
+  const onBuyPacksButtonPress = () => {
+    router.push('/(drawer)/StoreScreen');
+  };
+
   // Check user document's cards subcollection to get collectionCount
 
   return (
@@ -17,7 +27,19 @@ const CollectionOverview = ({ className = '' }: CollectionOverviewProps) => {
       className={`border-[3px] border-primary h-[200px] bg-background-2 rounded-[20px] pt-[22px] p-[20px] 
       ${className}`}
     >
-      <OpenPacksButton />
+      <View className='flex-row'>
+        <IconButtonTwoRows
+          className='mr-[10px]'
+          row1={'Open'}
+          row2={'Packs'}
+          onButtonPress={onOpenPacksButtonPress}
+        />
+        <IconButtonTwoRows
+          row1={'Buy'}
+          row2={'Packs'}
+          onButtonPress={onBuyPacksButtonPress}
+        />
+      </View>
       <View className='flex-row justify-between items-center mt-[49px]'>
         <CollectionCountPill />
         <View className='flex-row'>
--- frontend/components/IconButtonTwoRows.tsx
+++ frontend/components/IconButtonTwoRows.tsx
@@ -1,18 +1,32 @@
-import { View, Image, Text, TouchableOpacity, GestureResponderEvent } from 'react-native';
+import {
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  GestureResponderEvent,
+} from 'react-native';
 import React from 'react';
 import icons from '@/constants/icons';
 import colors from '@/constants/colors';
 
 interface OpenPacksButtonProps {
   className?: string;
+  row1?: string;
+  row2?: string;
   onButtonPress?: (event: GestureResponderEvent) => void | undefined;
 }
 
-const OpenPacksButton = ({className = '', onButtonPress} : OpenPacksButtonProps) => {
+const IconButtonTwoRows = ({
+  className = '',
+  row1 = 'Row 1',
+  row2 = 'Row 2',
+  onButtonPress,
+}: OpenPacksButtonProps) => {
   return (
-    <TouchableOpacity onPress={onButtonPress}
-      className='flex-row w-[120px] h-[63px] bg-background border-[3px] border-primary 
-        items-center justify-center rounded-[10px] rounded-tl-[20px] rounded-br-[20px] px-[18px] py-[7px]'
+    <TouchableOpacity
+      onPress={onButtonPress}
+      className={`flex-row w-[120px] h-[63px] bg-background border-[3px] border-primary 
+        items-center justify-center rounded-[10px] rounded-tl-[20px] rounded-br-[20px] px-[18px] py-[7px] ${className}`}
     >
       <Image
         source={icons.cards}
@@ -21,14 +35,14 @@ const OpenPacksButton = ({className = '', onButtonPress} : OpenPacksButtonProps)
       />
       <View>
         <Text className='text-text font-bold text-[16px]/[17px] ml-[8px]'>
-          Open
+          {row1}
         </Text>
         <Text className='text-text font-bold text-[16px]/[17px] ml-[8px]'>
-          Packs
+          {row2}
         </Text>
       </View>
     </TouchableOpacity>
   );
 };
 
-export default OpenPacksButton;
+export default IconButtonTwoRows;
