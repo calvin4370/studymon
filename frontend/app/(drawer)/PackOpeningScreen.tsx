9ec07afdb4a671e97fb599ec9697d6575afcf798
calvin4370 <calvin.j.j.chan@gmail.com>
2025-06-30 01:24:21 +0800

    feat(collection): Add collectionOnly view of all owned cards

---
+++ frontend/app/(drawer)/PackOpeningScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PackOpeningScreen = () => {
+  return (
+    <View>
+      <Text>PackOpeningScreen</Text>
+    </View>
+  )
+}
+
+export default PackOpeningScreen
