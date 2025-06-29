c0ac3b5213657ba6a580a2bfbfe6f9fa6627d394
calvin4370 <calvin.j.j.chan@gmail.com>
2025-06-30 03:51:44 +0800

    feat(open-packs): Add Owned Packs View and TouchableOpacities to access packs

--- frontend/app/(drawer)/PackOpeningScreen.tsx
+++ frontend/app/(drawer)/PackOpeningScreen.tsx
@@ -1,12 +1,90 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { FIREBASE_AUTH, FIREBASE_DATABASE } from '@/firebaseConfig';
+import NumOwnedPill from '@/components/NumOwnedPill';
+import PackCard from './PackCard';
 
 const PackOpeningScreen = () => {
-  return (
-    <View>
-      <Text>PackOpeningScreen</Text>
-    </View>
-  )
-}
+  // States
+  const [ownedPacks, setOwnedPacks] = useState<any[]>([]); // Quickfix, Figure out what to do with the any type later
+  const [openedCards, setOpenedCards] = useState<any[]>([]); // Quickfix, Figure out what to do with the any type later
 
-export default PackOpeningScreen
+  // Fetch all  user's ownedPacks from Firestore database
+  useEffect(() => {
+    const fetchPackData = async () => {
+      try {
+        // Fetch all owned packs for users->userId->packs subcollection
+        const userId = FIREBASE_AUTH.currentUser?.uid;
+        if (userId) {
+          const ownedPacksRef = collection(
+            FIREBASE_DATABASE,
+            'users',
+            userId,
+            'packs'
+          );
+          const ownedPacksSnap = await getDocs(ownedPacksRef);
+          const fetchedOwnedPacks = ownedPacksSnap.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          setOwnedPacks(fetchedOwnedPacks);
+        } else {
+          console.warn('Cannot find userId for current user');
+          setOwnedPacks([]);
+        }
+      } catch (error: any) {
+        console.error('Error fetching user ownedPacks data: ', error);
+      } finally {
+        console.log("Fetched user's ownedPacks");
+      }
+    };
+
+    fetchPackData();
+  }, []); // Dependency array empty so that this only runs once when component mounts
+
+  // Function definitions for Buttons
+
+  return (
+    <View className='p-[20px] '>
+      <View>
+        <Text className='text-text font-bold text-[30px] mb-[20px]'>
+          Your Packs ({ownedPacks.length} owned)
+        </Text>
+        <FlatList
+          className='p-[20px] border-[2px] rounded-[20px]'
+          data={ownedPacks}
+          renderItem={({ item }) => {
+            return (
+              <View className='items-center'>
+                {/* Update PackCard to take other packs by setCode and packTier */}
+                <PackCard numOwned={ownedPacks.length} onPackPress={() => {alert('TBD')}}/>
+              </View>
+            );
+          }}
+          keyExtractor={(item) => item.setCode.toString()}
+          horizontal={true}
+        />
+      </View>
+      <View>
+        <Text className='text-text font-bold text-[30px] mt-[20px] mb-[20px]'>
+          Opened Cards
+        </Text>
+        {openedCards.length > 0 ? (
+          <View>
+            <Text>Opened cards: {openedCards}</Text>
+          </View>
+        ) : (
+          // No opened cards i.e. have not opened packs yet
+          <View className='h-[200px] items-center justify-center'>
+            <Text className='text-text font-medium text-[24px]'>No opened cards</Text>
+            <Text className='text-text font-medium text-[24px]'>Open cards from packs</Text>
+          </View>
+        )}
+      </View>
+      
+    </View>
+  );
+};
+
+export default PackOpeningScreen;
---
+++ frontend/components/OpenedCard.tsx
@@ -0,0 +1,51 @@
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { cardArtMap } from '@/assets/cards/CardArtMap';
+
+interface OpenedCardProps {
+  cardNum: string;
+  initialFace: string;
+}
+
+const OpenedCard = ({ cardNum, initialFace = 'down' }: OpenedCardProps) => {
+  const cardBackImageSource = require('assets/cards/card-back.png');
+  const cardImageSource = cardArtMap[`assets/cards/${cardNum}.png`]; // Only works for BASE set for now, add ${item.setCode} later
+
+  // States
+  const [face, setFace] = useState(initialFace);
+
+  // Function Definitions
+  const handleCardPress = () => {
+    setFace(face === 'down' ? 'up' : 'down');
+  };
+
+  return (
+    <View className=''>
+      {face === 'down' ? (
+        <TouchableOpacity onPress={handleCardPress}>
+          <Image
+            source={cardBackImageSource}
+            className='w-[126px] h-[176px]'
+            width={126}
+            height={176}
+            resizeMode='cover'
+          />
+          <Text>{face}</Text>
+        </TouchableOpacity>
+      ) : (
+        <View>
+          <Image
+            source={cardImageSource}
+            className='w-[126px] h-[176px]'
+            width={126}
+            height={176}
+            resizeMode='cover'
+          />
+          <Text>{face}</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default OpenedCard;
